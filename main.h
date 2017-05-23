01b2c05cbb7299241571f7f039a591e09088aeec
Eric S. Raymond <esr@thyrsus.com>
2017-05-22 20:51:55 -0400

    More magic-number elimination.

--- main.h
+++ main.h
@@ -1,6 +1,6 @@
-#define LINESIZE	101
+#define LINESIZE	100
 
 extern long ABB[], ATAB[], ATLOC[], BLKLIN, DFLAG, DLOC[], FIXED[], HOLDNG,
 		KTAB[], *LINES, LINK[], LNLENG, LNPOSN,
 		PARMS[], PLACE[], PTEXT[], RTEXT[], TABSIZ;
-extern signed char INLINE[LINESIZE], MAP1[], MAP2[];
+extern signed char INLINE[LINESIZE+1], MAP1[], MAP2[];
