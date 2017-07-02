2d71fe813793334a0e2c46d8b942dba9a5aff208
Aaron Traas <aaron@traas.org>
2017-07-02 12:26:04 -0400

    Add capabilities to cheat. Expand testing to cover.

--- cheat.c
+++ cheat.c
@@ -14,29 +14,48 @@ int main(int argc, char *argv[])
 {
     int ch;
     char *savefilename = NULL;
-    long numdie = 0;
-    long saved = 1;
     long version = 0;
     FILE *fp = NULL;
 
+    // Initialize game variables
+    initialise();
+    
+    /* we're generating a saved game, so saved once by default, 
+     * unless overridden with command-line options below.
+     */
+    game.saved = 1;
+
     /*  Options. */
-    const char* opts = "d:s:v:o:";
+    const char* opts = "d:l:s:t:v:o:";
     const char* usage = "Usage: %s [-d numdie] [-s numsaves] [-v version] -o savefilename \n"
-                        "        -d number of deaths. Signed integer value.\n"
-                        "        -s number of saves. Signed integer value.\n"
+                        "        -d number of deaths. Signed integer.\n"
+                        "        -l lifetime of lamp in turns. Signed integer.\n"
+                        "        -s number of saves. Signed integer.\n"
+                        "        -t number of turns. Signed integer.\n"
                         "        -v version number of save format.\n"
                         "        -o required. File name of save game to write.\n";
 
     while ((ch = getopt(argc, argv, opts)) != EOF) {
         switch (ch) {
         case 'd':
-            numdie = (long)atoi(optarg);
+            game.numdie = (long)atoi(optarg);
+            printf("cheat: game.numdie = %ld\n", game.numdie);
+            break;
+        case 'l':
+            game.limit = (long)atoi(optarg);
+            printf("cheat: game.limit = %ld\n", game.limit);
             break;
         case 's':
-            saved = (long)atoi(optarg);
+            game.saved = (long)atoi(optarg);
+            printf("cheat: game.saved = %ld\n", game.saved);
+            break;
+        case 't':
+            game.turns = (long)atoi(optarg);
+            printf("cheat: game.turns = %ld\n", game.turns);
             break;
         case 'v':
-            version = (long)atoi(optarg);;
+            version = (long)atoi(optarg);
+            printf("cheat: version = %ld\n", version);
             break;
         case 'o':
             savefilename = optarg;
@@ -58,13 +77,6 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    // Initialize game variables
-    initialise();
-
-    // apply cheats
-    game.numdie = numdie;
-    game.saved = saved;
-
     fp = fopen(savefilename, WRITE_MODE);
     if (fp == NULL) {
         fprintf(stderr,
@@ -73,6 +85,8 @@ int main(int argc, char *argv[])
     }
 
     savefile(fp, version);
+
+    fclose(fp);
 
     printf("cheat: %s created.\n", savefilename);
 
